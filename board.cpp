d1220b0fb9c994faad0671a1655871aa0225d7d8
Matt Hackmann <matt@dxprog.com>
2015-06-04 22:31:19 -0700

    added Wheel from NeoPixel lib

--- board.cpp
+++ board.cpp
@@ -36,3 +36,18 @@ void render() {
 uint32_t createColor(uint8_t r, uint8_t g, uint8_t b) {
   return pixels.Color(r, g, b);
 }
+
+
+// Taken from NeoPixel library
+uint32_t Wheel(uint8_t WheelPos) {
+  WheelPos = 255 - WheelPos;
+  if(WheelPos < 85) {
+   return pixels.Color(255 - WheelPos * 3, 0, WheelPos * 3);
+  } else if(WheelPos < 170) {
+    WheelPos -= 85;
+   return pixels.Color(0, WheelPos * 3, 255 - WheelPos * 3);
+  } else {
+   WheelPos -= 170;
+   return pixels.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
+  }
+}
--- board.h
+++ board.h
@@ -16,5 +16,6 @@ void setPixel(int x, int y, int r, int g, int b);
 void setPixel(int x, int y, uint32_t color);
 void render();
 uint32_t createColor(uint8_t r, uint8_t g, uint8_t b);
+uint32_t Wheel(uint8_t WheelPos);
 
 #endif __BOARD_H__
