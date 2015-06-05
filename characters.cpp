6334ed689dd891d3432f165821825a25243ff9de
Matt Hackmann <matt@dxprog.com>
2015-06-04 22:31:58 -0700

    added color option to drawString, changed other function sigs

--- characters.cpp
+++ characters.cpp
@@ -8,39 +8,43 @@
 #define SPACE_WIDTH 4
 #define CHAR_WIDTH  8
 
-void drawCharacter(int character, int x, int y, int r, int g, int b) {
+void drawCharacter(int character, int x, int y, uint32_t color) {
   for (int i = 0; i < 8; i++) {
     uint8_t row = CHARACTERS[character][i];
     for (int j = 0; j < 8; j++) {
       uint8_t offset = 7 - j;
       uint8_t pixel = (row >> offset) & 1;
       if (pixel > 0) {
-        setPixel(x + j, y + i, r, g, b);
+        setPixel(x + j, y + i, color);
       } else {
-        setPixel(x + j, y + i, 0, 0, 0);
+        setPixel(x + j, y + i, 0);
       }
     }
   }
 }
 
-void drawNumber(int number, int r, int g, int b) {
+void drawNumber(int number, uint32_t color) {
   int right = GRID_WIDTH - 8;
   while (number > 0) {
     int digit = number % 10;
-    drawCharacter(digit, right, 1, r, g, b);
+    drawCharacter(digit, right, 1, color);
     number = (int) number / 10;
     right -= 8;
   }
 }
 
-void drawString(char *string, int16_t xOffset, int16_t yOffset) {
+void drawString(char *string, int16_t xOffset, int16_t yOffset, uint32_t color) {
 
-  uint16_t width = 0;
+  uint32_t width = 0;
 
   for (uint16_t i = 0, len = strlen(string); i < len; i++) {
 
-    int16_t x = xOffset + width;
+    int32_t x = xOffset + width;
     uint8_t characterWidth = 0;
+
+    if (x > GRID_WIDTH) {
+      break;
+    }
 
     if (string[i] == 32) {
       characterWidth = SPACE_WIDTH;
@@ -50,14 +54,11 @@ void drawString(char *string, int16_t xOffset, int16_t yOffset) {
 
     // Determine if the character is within the drawable field
     // and if it's a drawable character
-    if (x + characterWidth > -1 && characterWidth > SPACE_WIDTH) {
-      drawCharacter(string[i] - 33, x, yOffset, 80, 0, 0);
+    if (x + characterWidth > -1 && x < GRID_WIDTH && characterWidth > SPACE_WIDTH) {
+      drawCharacter(string[i] - 33, x, yOffset, color);
     }
 
     width += characterWidth;
-    if (width > GRID_WIDTH) {
-      break;
-    }
 
   }
 
--- characters.h
+++ characters.h
@@ -1,8 +1,8 @@
 #ifndef __CHARACTERS_H__
 
-void drawCharacter(int character, int x, int y, int r, int g, int b);
-void drawNumber(int number, int r, int g, int b);
-void drawString(char *string, int16_t xOffset, int16_t yOffset);
+void drawCharacter(int character, int x, int y, uint32_t color);
+void drawNumber(int number, uint32_t color);
+void drawString(char *string, int16_t xOffset, int16_t yOffset, uint32_t color);
 
 #define __CHARACTERS_H__
 #endif
